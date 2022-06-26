68ac8b69f1eb723707c282a575620960debf0a70
krishna <63730916+krish-96@users.noreply.github.com>
2022-06-26 18:57:39 +0530

    search app created and just started working on views

--- NewsLetter_app/forms.py
+++
@@ -1,7 +0,0 @@
-# from .models import NewsLetter
-# from django.forms import ModelForm
-#
-# class NewsLetterForm(ModelForm):
-#     class Meta:
-#         model = NewsLetter
-#         fields = "__all__"
